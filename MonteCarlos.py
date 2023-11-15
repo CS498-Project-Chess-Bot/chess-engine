bcae5385eccdfba154bc275ac25d51b82469bf28
eptate01 <ethanptate@gmail.com>
2023-11-15 15:59:52 -0500

    Improved Time and quality of Moves

--- MonteCarlos.py
+++ MonteCarlos.py
@@ -3,6 +3,7 @@ import math
 import random
 from copy import deepcopy
 import os
+import time
 
 
 class Node():
@@ -20,6 +21,7 @@ class Node():
         self.visits = 0 #amount of visits to node when created is 0
         self.score = 0 #amount of score is 0 when node is created
         self.children = {}
+        self.childMovesNotExplored = list(self.board.legal_moves)
         
 
 class Tree():
@@ -30,46 +32,52 @@ class Tree():
         #limit search by iterations
         for iteration in range(self.difficulty):
             #choose a node
+            #start_time_expansion = time.time()
             node = self.selectExpansion(self.root)
-
-            #score the node by 
-            score = self.simulation(deepcopy(node.board))
-
+            #print("--- %s for select Expansion ---" % (time.time() - start_time_expansion))
+            #score the node by
+            #start_time_simulation = time.time()
+            score = self.simulation(node.board)
+            #print("--- %s for select Simulation ---" % (time.time() - start_time_simulation))
             #update scores and visits for nodes along path
+            #start_time_backprop = time.time()
             self.backProp(node, score)
-        
+            #print("--- %s for select backprop ---" % (time.time() - start_time_backprop))
+
         bestMove = self.chooseBestMove(self.root, 0).board
         self.root = self.root.children[bestMove.fen()]
         return bestMove 
 
     def selectExpansion(self, node):
-        while node.board.outcome() == None:
-            if node.fullyExpanded:
-                node = self.selectExpansion(self.chooseBestMove(node, 2)) #make this select expansion and see what happens
-            else:
+        if node.board.outcome() == None:
+            if node.fullyExpanded: 
+                return self.selectExpansion(self.chooseBestMove(node, 1.5)) #make this select expansion and see what happens
+            else: 
                 return self.expand(node)
         else: return node
 
-    def expand(self, node):
-        legalMoves = list(node.board.legal_moves)
-        for move in legalMoves:
-            tempBoard = deepcopy(node.board)
-            tempBoard.push(move)
-            if tempBoard.fen() not in node.children.keys():
-                new_node = Node(tempBoard, node)
-                node.children[tempBoard.fen()] = new_node
+    def expand(self, node: Node):
+        move = random.choice(node.childMovesNotExplored)
+        tempBoard = deepcopy(node.board)
+        tempBoard.push(move)
+        new_node = Node(tempBoard, node)
+        node.children[tempBoard.fen()] = new_node
+        node.childMovesNotExplored.remove(move)
 
-                if len(legalMoves) == len(node.children):
-                    node.fullyExpanded = True
+        if not node.childMovesNotExplored: node.fullyExpanded = True
 
-                return new_node
-        print('Error: no node found in expand function')
+        return new_node
 
     def simulation(self, board):
+        
         tempBoard = deepcopy(board)
+    
+        #start_time_expansion = time.time()
         while tempBoard.outcome() == None:
             legalMoves = list(tempBoard.legal_moves)
-            tempBoard.push(random.choice(legalMoves))
+            #randInt = random.randint(0,len(legalMoves)-1)
+            tempBoard.push(legalMoves[-1])
+       # print("--- %s for loop ---" % (time.time() - start_time_expansion))
         
         if tempBoard.is_checkmate():
             if tempBoard.turn:
@@ -96,23 +104,23 @@ class Tree():
             elif child.board.turn == False: current_player = -1
             move_score = current_player*child.score/child.visits + explorationConstant * math.sqrt(math.log(node.visits/child.visits))
 
-            if move_score > highScore:
+            if move_score > highScore or highScore == -99999:
                 highScore = move_score
                 bestMoves = [child]
             elif move_score == highScore:
                 bestMoves.append(child)
             
-        return random.choice(bestMoves)
+        return bestMoves[0]
             
 
 def Move(FEN, diff=1):
     board = chess.Board(FEN)
-    if diff == 1: difficulty = 10000
+    if diff == 1: difficulty = 10001
     elif diff == 2: difficulty = 800
     else: difficulty = 600
 
     tree = Tree(board, difficulty)
-
+    
     return tree.chooseMove()
 
 
