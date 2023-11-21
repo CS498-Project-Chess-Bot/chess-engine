81023c7c5d0004b11b86cd1664b16180af4094ef
eptate01 <ethanptate@gmail.com>
2023-11-21 14:40:48 -0500

    .bat FIle Created

--- BuildEngine.bat
+++ BuildEngine.bat
@@ -1,5 +1,11 @@
-pip install -q pyinstaller;
-pip install -q chess;
-pip install -q random;
+pip install -q pyinstaller
+pip install -q chess
+pip install -q random
 
-python -m PyInstaller --noconfirm .\MoveGenerator.py;
+python -m PyInstaller --noconfirm MoveGenerator.py
+
+MOVE .\dist\MoveGenerator\MoveGenerator.exe .\
+
+rmdir /s /q .\dist;
+rmdir /s /q .\build;
+del MoveGenerator.spec
